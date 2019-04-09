f69c435f935841b7f0dfcc4bec6963444651f73f
Polarbear72 <polarbear72@web.de>
2019-04-09 12:40:53 +0200

    Split in two tabs
    Separated initalization and setup from loop.

---
+++ Temperaturmesser/Temperaturmesser_Loop.ino
@@ -0,0 +1,34 @@
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
+  if (temperature < baselineTemp) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    delay(1000);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    delay(1000);
+  } else if (temperature >= baselineTemp && temperature < baselineTemp + 2) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } else if (temperature > baselineTemp + 4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+//  delay(1000);
+}
